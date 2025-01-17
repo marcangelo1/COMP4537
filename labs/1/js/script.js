691152b5e7b86e4ab8736db1722e82aa26bfd45c
Marc <144364703+marcangelo1@users.noreply.github.com>
2025-01-17 10:23:10 -0800

    Put the text of index.html to en user.js

--- labs/1/js/script.js
+++ labs/1/js/script.js
@@ -1,7 +1,16 @@
+import { indexTexts } from "../lang/messages/en/user.js";
+
 document.addEventListener("DOMContentLoaded", () => {
 
   const writerButton = document.getElementById("writer-button");
   const readerButton = document.getElementById("reader-button");
+  const title = document.getElementById("title");
+  const info = document.getElementById("info");
+
+  title.textContent = indexTexts.title;
+  info.textContent = indexTexts.info;
+  writerButton.textContent = indexTexts.writerButtonText;
+  readerButton.textContent = indexTexts.readerButtonText;
 
   writerButton.addEventListener("click", () => {
     location.href = "writer.html";
---
+++ labs/1/lang/messages/en/user.js
@@ -0,0 +1,6 @@
+export const indexTexts = {
+  title: "Welcome to the Note App",
+  info: "Let's create, save, and view notes.",
+  writerButtonText: "Go to Writer",
+  readerButtonText: "Go to Reader",
+};
